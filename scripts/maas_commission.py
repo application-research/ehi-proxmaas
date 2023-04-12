2ca9d4ee364a7ae56127a3bb2bfc0a22dc1af1bc
Benjamin Arntzen <zorlin@gmail.com>
2023-04-12 01:31:18 -0500

    Working end-to-end script!!

--- scripts/maas_commission.py
+++ scripts/maas_commission.py
@@ -15,8 +15,8 @@ parser.add_argument('--vm_details', type=str, required=True, help='VM details fr
 # Parse the command-line arguments
 args = parser.parse_args()
 
-# Write a copy of the VM details to a file
-with open("vm_details.json", "w") as f:
+# DEBUG: Write a copy of the VM details to a file
+with open("/root/vm_details.json", "w") as f:
     f.write(args.vm_details)
     f.close
 
@@ -37,26 +37,26 @@ for machine in client.machines.list():
 # Iterate through the data in vm_details
 target_mac_address = None
 
-vm_details = json.load(args.vm_details)
-for vm in vm_details:
-    print(vm.mac)
-    print(vm.name)
-    # for mac in vm_details[vm]["mac"]:
-    #     if mac in machine_mac_addresses:
-    #         target_mac_address = mac
+# Load the VM details from the JSON object we were passed
+vm_details = json.loads(args.vm_details)
 
-# For machine with this MAC
-print(machine_mac_addresses[target_mac_address])
-machine = client.machines.get(system_id=machine_mac_addresses[target_mac_address]["system_id"])
-machine.hostname = "dev-test01"
-machine.power_type="proxmox"
-machine.set_power(power_type="proxmox", power_parameters={
+# For machine with this MAC, set all the correct parameters
+for vm_key in vm_details:
+    vm = vm_details[vm_key]
+    target_mac_address = vm["mac"]["net0"].lower()
+    machine = client.machines.get(system_id=machine_mac_addresses[target_mac_address]["system_id"])
+    machine.hostname = vm["name"]
+    # machine.power_type="proxmox"
+    machine.set_power(power_type="proxmox", power_parameters={
     "power_address": "https://proxmox.estuary.tech:8006",
     "power_user": args.proxmox_user,
     "power_token_name": args.proxmox_token_id,
     "power_token_secret": args.proxmox_token_secret,
-    "power_vm_name": 133,
+    "power_vm_name": vm_key,
     "power_verify_ssl": "n"
-})
-
-machine.save()
+    })
+    machine.save()
+    # Check the power on the machine to update its BMC status
+    machine.query_power_state()
+    # Commission the machine
+    machine.commission(commissioning_scripts=[])
---
+++ scripts/maas_deploy.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import argparse
+import json
+from maas.client import connect
+import configparser
+from os.path import expanduser
+
+# Set up the argument parser
+parser = argparse.ArgumentParser(description="Set Proxmox token ID and secret for a machine in MAAS")
+parser.add_argument('--vm_details', type=str, required=True, help='VM details from Ansible')
+
+# Parse the command-line arguments
+args = parser.parse_args()
+
+# Set up the config parser
+config = configparser.ConfigParser()
+config.read(expanduser("~/.proxmox-maas.cfg"))
+
+# Connect to MAAS
+client = connect("https://maas.estuary.tech/MAAS", apikey=config["maas_api"]["api_key"])
+
+# Get a list of all machines and their MAC addresses
+machine_mac_addresses = {}
+for machine in client.machines.list():
+    for interface in machine.interfaces:
+        machine_mac_addresses[interface.mac_address] = { "system_id": machine.system_id, "name": machine.hostname }
+
+# Iterate through the data in vm_details
+target_mac_address = None
+
+# Load the VM details from the JSON object we were passed
+vm_details = json.loads(args.vm_details)
+
+# For machine with this MAC, set all the correct parameters
+for vm_key in vm_details:
+    vm = vm_details[vm_key]
+    target_mac_address = vm["mac"]["net0"].lower()
+    machine = client.machines.get(system_id=machine_mac_addresses[target_mac_address]["system_id"])
+    # Check the power on the machine to update its BMC status
+    machine.query_power_state()
+    # Deploy the machine
+    machine.deploy()
